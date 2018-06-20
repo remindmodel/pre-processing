809894ebd1ed1e59bb106ee4e2e396864ea085c1
David Klein <dklein@pik-potsdam.de>
2018-06-20 17:04:20 +0200

    Added first file

---
+++ start.R
@@ -0,0 +1,12 @@
+library(moinput)
+
+cfg <- list()
+
+#### region mapping ####
+cfg$regionmapping <- "regionmappingH12.csv"
+
+#### Current input data revision (<mainrevision>.<subrevision>) ####
+cfg$revision <- 5.681
+
+retrieveData(model="REMIND",regionmapping=cfg$regionmapping,rev=cfg$revision)
+ 
