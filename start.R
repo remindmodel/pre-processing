1e96b38e6f9eb9882148edb14493a2a076599e7a
Lavinia Baumstark <baumstark@pik-potsdam>
2018-07-20 10:04:17 +0200

    changed the current input data revision

--- start.R
+++ start.R
@@ -6,7 +6,7 @@ cfg <- list()
 cfg$regionmapping <- "regionmappingH12.csv"
 
 #### Current input data revision (<mainrevision>.<subrevision>) ####
-cfg$revision <- 5.681
+cfg$revision <- 5.69
 
 retrieveData(model="REMIND",regionmapping=cfg$regionmapping,rev=cfg$revision)
  
