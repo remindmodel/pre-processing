bdb2e74f578bb4ad980fd4f0d5c1553aeaf38b6d
David Klein <dklein@pik-potsdam.de>
2018-11-20 14:25:42 +0100

    Increased revision number

--- start.R
+++ start.R
@@ -6,7 +6,7 @@ cfg <- list()
 cfg$regionmapping <- "regionmappingH12.csv"
 
 #### Current input data revision (<mainrevision>.<subrevision>) ####
-cfg$revision <- 5.803
+cfg$revision <- 5.805
 
 retrieveData(model="REMIND",regionmapping=cfg$regionmapping,rev=cfg$revision)
  
