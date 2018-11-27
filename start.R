b2a891c034893af1b431a24d14ba3c3d4ad1f93c
AL <levesque@pik-potsdam.de>
2018-11-27 17:52:35 +0100

    raised revision numbered

--- start.R
+++ start.R
@@ -6,7 +6,7 @@ cfg <- list()
 cfg$regionmapping <- "regionmappingH12.csv"
 
 #### Current input data revision (<mainrevision>.<subrevision>) ####
-cfg$revision <- 5.807
+cfg$revision <- 5.808
 
 retrieveData(model="REMIND",regionmapping=cfg$regionmapping,rev=cfg$revision)
  
