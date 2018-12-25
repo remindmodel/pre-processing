5b778edb18586d252187b23ccbe4c74514bc880a
Lavinia Baumstark <baumstark@pik-potsdam>
2018-12-25 13:53:33 +0100

    added regionmapping for separated Australia

--- start.R
+++ start.R
@@ -1,7 +1,7 @@
 library(moinput)
 
 #### list of region mappings to create input data ####
-regionmappinglist <- c("regionmappingH12.csv","regionmapping_16_EU5.csv","regionmapping_22_EU11.csv")
+regionmappinglist <- c("regionmappingH12.csv","regionmapping_16_EU5.csv","regionmapping_22_EU11.csv","regionmappingH12_Aus.csv")
 
 
 #### Current input data revision (<mainrevision>.<subrevision>) ####
