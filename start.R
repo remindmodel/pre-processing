7accd384dad7d78939c0c89f07622dc36b5334d2
Renato Rodrigues <renatoro@pik-potsdam.de>
2019-04-01 16:34:41 +0200

    * updating REMIND-EU mapping. * increasing revision number

--- start.R
+++ start.R
@@ -1,11 +1,11 @@
 library(moinput)
 
 #### list of region mappings to create input data ####
-regionmappinglist <- c("regionmappingH12.csv","regionmapping_16_EU5.csv","regionmapping_22_EU11.csv","regionmappingH12_Aus.csv")
+regionmappinglist <- c("regionmappingH12.csv","regionmapping_16_EU6.csv","regionmapping_21_EU11.csv","regionmappingH12_Aus.csv")
 
 
 #### Current input data revision (<mainrevision>.<subrevision>) ####
-revision <- 5.902
+revision <- 5.835
 
 for (regionmapping in regionmappinglist){
    retrieveData(model="REMIND",regionmapping=regionmapping,rev=revision)
