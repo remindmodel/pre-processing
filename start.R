ad93d6b30100ea4d955bc40ca6165ac0af9ca49a
AL <levesque@pik-potsdam.de>
2019-07-08 13:59:50 +0200

    updated revision number

--- start.R
+++ start.R
@@ -5,7 +5,7 @@ regionmappinglist <- c("regionmappingH12.csv","regionmapping_21_EU11.csv","regio
 
 
 #### Current input data revision (<mainrevision>.<subrevision>) ####
-revision <- 5.847
+revision <- 5.848
 
 for (regionmapping in regionmappinglist){
    retrieveData(model="REMIND",regionmapping=regionmapping,rev=revision)
