ae30920954d312748185aa9c5a9bc3bed383e277
Michaja Pehl <pehl@pik-potsdam.de>
2019-07-18 15:37:20 +0200

    generate input for REMIND mapping, too

--- start.R
+++ start.R
@@ -1,7 +1,8 @@
 library(moinput)
 
 #### list of region mappings to create input data ####
-regionmappinglist <- c("regionmappingH12.csv","regionmapping_21_EU11.csv","regionmapping_22_EU11.csv","regionmappingH12_Aus.csv")
+regionmappinglist <- c('regionmappingREMIND.csv',
+                       "regionmappingH12.csv","regionmapping_21_EU11.csv","regionmapping_22_EU11.csv","regionmappingH12_Aus.csv")
 
 
 #### Current input data revision (<mainrevision>.<subrevision>) ####
