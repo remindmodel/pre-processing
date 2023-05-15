3692ea870e9917f581d9c7bf80e084aa26435b6d
Lavinia Baumstark <baumstark@pik-potsdam.de>
2023-05-15 15:01:08 +0200

    refactoring: separate configuration from function

--- start.R
+++ start.R
@@ -1,40 +1,34 @@
-library(mrremind)
-library(mrcommons)
-library(mrvalidation)
-library(edgeTransport)
 
-# List with the names of the files containing the ISO-to-region mapping to which the data should be aggregated. 
-# Each list element is a named vector with:
-# - the first element "regionmapping": region mapping used for the aggregation of the input data
-# - the second element "extramappings_historic": mapping with regions to which only the historic data are aggregated in addition to the normal region mapping
+start_preprocessing <- function(cfg) {
 
-mappinglist <- list(c(regionmapping = "regionmappingH12.csv",      extramappings_historic = ""),
-                    c(regionmapping = "regionmapping_21_EU11.csv", extramappings_historic = "extramapping_EU27.csv"))
+  library(mrremind)
+  library(mrcommons)
+  library(mrvalidation)
+  library(edgeTransport)
 
-# Current input data revision (<mainrevision>.<subrevision>) ####
-revision <- "6.52"   # should be a number with two decimal places for production
-dev <- ""             # development suffix
-cachetype <- "def"
-puc <- identical(dev, '')
-renv <- TRUE
-
-sessionInfo()
-
-# disable size limit on magpie objects
-options(magclass_sizeLimit = -1)
-
-for (mapping in mappinglist) {
-
-  # Produce input data for all regionmappings (ignore extramappings_historic)  
-  retrieveData(model = "REMIND", regionmapping = mapping[["regionmapping"]], 
-               rev = revision, dev = dev, cachetype = cachetype, puc = puc,
-               renv = renv)
+  message(sessionInfo())
   
-  # Produce historical data for regionmappings and extramappings_historic.
-  # The region hash of the historical data file will concatenated from the individual hashes of regionmapping and extramappings_historic.
-  retrieveData(model = "VALIDATIONREMIND", 
-               regionmapping = mapping[["regionmapping"]], 
-               extramappings = mapping[["extramappings_historic"]], 
-               rev = revision, dev = dev, cachetype = cachetype, puc = puc,
-               renv = renv)
+  # disable size limit on magpie objects
+  options(magclass_sizeLimit = -1)
+  
+  # read in configuration
+  cfg <- gms::check_config(cfg, modulepath = NULL)
+  
+  for (mapping in cfg$mappinglist) {
+  
+    # Produce input data for all regionmappings (ignore extramappings_historic)  
+    retrieveData(model = "REMIND", regionmapping = mapping[["regionmapping"]], 
+                 rev = cfg$revision, dev = cfg$dev, cachetype = cfg$cachetype,
+                 renv = cfg$renv)
+    
+    # Produce historical data for regionmappings and extramappings_historic.
+    # The region hash of the historical data file will concatenated from the individual hashes of regionmapping and extramappings_historic.
+    retrieveData(model = "VALIDATIONREMIND", 
+                 regionmapping = mapping[["regionmapping"]], 
+                 extramappings = mapping[["extramappings_historic"]], 
+                 rev = cfg$revision, dev = cfg$dev, cachetype = cfg$cachetype,
+                 renv = cfg$renv)
+  }
 }
+
+start_preprocessing(cfg = "default.cfg")
