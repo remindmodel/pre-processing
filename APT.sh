3b34d076e63475f6b3f87843286b92490634fee7
Lavinia Baumstark <baumstark@pik-potsdam.de>
2023-06-01 09:51:13 +0200

    delete APT script

--- APT.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /p/projects/rd3mod/cron_setup.sh
-
-cd /p/projects/rd3mod/APT/preprocessing-remind
-git pull
-Rscript -e 'renv::hydrate(rownames(installed.packages()), update = "all")'
-
-Rscript submit_preprocessing.R config/APT.cfg
